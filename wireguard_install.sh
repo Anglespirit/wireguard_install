b71f72dffbd68d947b0d12382301da65d7769ba2
黑夜 <1159683651@qq.com>
2018-11-20 22:11:28 +0800

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -20,7 +20,7 @@ Separator_1="——————————————————————�
 
 #检查root权限
 Check_root(){
-	[[ $EUID != 0 ]] && echo -e "${Error} 当前账号非ROOT(或没有ROOT权限)，无法继续操作，请使用${Green_background_prefix} sudo su ${Font_color_suffix}来获取临时ROOT权限（执行后会提示输入当前账号的密码）。" && exit 1
+	[[ $EUID!=0 ]] && echo -e "${Error} 当前账号非ROOT(或没有ROOT权限)，无法继续操作，请使用${Green_background_prefix} sudo su ${Font_color_suffix}来获取临时ROOT权限（执行后会提示输入当前账号的密码）。" && exit 1
 }
 
 #检测系统
